f409a78c446bcc36987fb213600869e8196a8177
Norix10 <norix123321@gmail.com>
2025-01-09 02:19:16 +0200

    can choose teacher in Lesson

--- sh_proj/main/models.py
+++ sh_proj/main/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+       return f"{self.first_name} {self.middle_name} {self.last_name}"
+
 class Lesson(models.Model):
     day_of_week = models.CharField(max_length=10, choices=[
         ('Monday', 'Понеділок'),
@@ -11,7 +19,7 @@ class Lesson(models.Model):
     ])
     time = models.TimeField()
     subject = models.CharField(max_length=100)
-    teacher = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='teach')
     room = models.CharField(max_length=50)
 
     def __str__(self):
